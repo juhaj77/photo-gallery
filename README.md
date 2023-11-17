22835a03372eb6dc34ad5fe7a2ac2a8e226b9391
juhaj77 <133148503+juhaj77@users.noreply.github.com>
2023-11-17 02:54:43 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,39 @@
+# The Image Gallery With Upload Feature
+Stores images to MongoDB utilizing multer. This solution is easy to extend to uploading multiple files at once. **_Todo: 1. gridFS for bigger than 16Mb pics. 2. elegant navigation for overflowed images._**
+
+## Prerequisites
+
+npm package manager and mongodb uri
+
+## Installing
+```
+/img-gallery-with-upload/server>npm install
+/img-gallery-with-upload/client>npm install
+```
+create `.env` file with content:
+```
+    MONGODB_URI=mongodb://127.0.0.1:27017
+    PORT=8000
+    NODE_ENV=test
+```
+__option A__ usage in production mode:
+1. set `NODE_ENV=production`
+2. build and run   
+```
+/img-gallery-with-upload/client>export NODE_OPTIONS=--openssl-legacy-provider
+/img-gallery-with-upload/client>npm run build
+/img-gallery-with-upload/client>cp -r build ../server
+/img-gallery-with-upload/server>npm start
+```
+and open http://localhost:8000/ in browser. ___Now folder ./server has all what you need to deploy this on a live system___
+
+__option B__ usage in development mode:
+```
+/img-gallery-with-upload/server>npm start
+/img-gallery-with-upload/client>export NODE_OPTIONS=--openssl-legacy-provider
+/img-gallery-with-upload/client>npm start
+```
+
+what it looks like:
+![kuvagalleria](https://github.com/juhaj77/img-gallery/blob/main/IGCapture.PNG)
+
---
+++ client/src/components/InfoPanel.js
@@ -0,0 +1,105 @@
+import React, { useReducer } from 'react'
+import styled from 'styled-components'
+import { useField } from '../hooks/field'
+
+const Container = styled.div`
+  border-radius: 0 0 0 1em;
+  white-space: nowrap;
+  height: 1.7em;
+  background-color: #800000;
+  color: white;
+  padding: 0;
+`
+const Content = styled.div`
+  height: 1.7em;
+  display: flex;
+  flex-flow: row-wrap;
+  font-size: 1em;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  align-content: center;
+  flex-wrap: nowrap;
+  overflow: hidden;
+  margin: 0;
+  padding: 0;
+  white-space: nowrap;
+`
+const LeftButton = styled.button`
+  align-items: center;
+  font-weight: bold;
+  height: 1.5em;
+  line-height: 1em;
+  margin: .2em;
+  padding:.1em .5em .1em .5em;
+  border-width: .01em;
+`
+const RightButton = styled.button`
+  align-items: center; 
+  font-weight: bold; 
+  height: 1.5em; 
+  line-height: 1em; 
+  margin: .2em .2em .2em 0; 
+  padding:.1em .5em .1em .5em; 
+  border-width: .01em;
+`
+const initial = { 
+  basic: { display: ''}, 
+  edit: { display:'none' } 
+}
+const reducer = state => state.basic.display === '' ? 
+{ basic: { display: 'none'}, edit: { display:'' } } :
+{ basic: { display: ''}, edit: { display:'none' } }
+
+const InfoPanel = ({ name, id, details, update, remove }) => {
+  const [style, toggle] = useReducer(reducer, initial)
+  const newName = useField('text', name)
+  const newDetails = useField('text', details)
+
+  const handleUpdate = () => {
+    update(id, newName.input.value, newDetails.input.value)
+    toggle(style)
+  }
+
+  return (
+    <Container>
+      <Content style={style.basic}>
+        <div style={{ margin: '0 0 .1em .7em', alignSelf: 'strech', lineHeight: '1em' }}>
+          {details}
+        </div>
+        <div style={{ alignItems: 'center' }}>
+          <LeftButton type="button" onClick={() => toggle(style)}>
+            edit
+          </LeftButton>
+          <RightButton type="button" onClick={() => remove(id)}>
+            delete
+          </RightButton>
+        </div>
+      </Content>
+      <Content style={style.edit}>
+        <div style={{alignSelf: 'strech', alignItems: 'center' }}>
+          <input
+            placeholder={name}
+            style={{ height: '1.6em', width: '5em' }}
+            {...newName.input}
+          />
+          <input
+            placeholder={details}
+            style={{ height: '1.6em', width: '7em' }}
+            {...newDetails.input}
+          />
+        </div>
+        <div style={{ alignSelf: 'strech', alignItems: 'center' }}>
+          <LeftButton type="button" onClick={handleUpdate}>
+            save
+          </LeftButton>
+          <RightButton type="button" onClick={() => toggle(style)}>
+            cancel
+          </RightButton>
+        </div>
+      </Content>
+    </Container>
+  )
+}
+
+export default InfoPanel
