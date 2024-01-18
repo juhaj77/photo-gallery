0bb85bc0c58b56d2d569f32e1c7b324ad35ed135
juhaj77 <juhaj358@gmail.com>
2024-01-18 14:47:25 +0200

    vertical align

--- client/src/components/MyImage.js
+++ client/src/components/MyImage.js
@@ -1,6 +1,48 @@
 /* eslint-disable react/destructuring-assignment */
 import React, { useState } from 'react'
 import InfoPanel from './InfoPanel'
+import styled from 'styled-components'
+
+const BackPlate = styled.div`
+  position: absolute;
+  top: 0;
+  background: black;
+  margin: auto;
+  zIndex: 999;
+  width: 100vw;
+  height: 100vh;
+`
+const Aligner = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  margin-left: -1em;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+const FittedImage = styled.img`
+  position: relative;
+  zIndex: 1000;
+  margin: auto;
+  max-width: calc(100vw - 2em);
+  max-height: 100vh;
+  height: auto;
+  cursor: pointer;
+`
+const TileImageContainer = styled.div`
+  width: fit-content;
+  text-align: center;
+  height: calc(100vh/3 - 4em/3);
+  margin: 1em 1em 0 0;
+`
+const TileImage = styled.img`
+  border-radius: 0 1em 0 0;
+  height: calc(100vh/3 - 4em/3 - 1.7em);
+  cursor: pointer;
+`
 
 const MyImage = (props) => {
 
@@ -31,49 +73,24 @@ const MyImage = (props) => {
 
   return fit ?
         <>
-          <div style={{ 
-            position:'absolute',
-            top:'0',
-            background:'black',
-            margin:'auto',
-            zIndex:'999',
-            width:'100vw',
-            height:'100vh',
-          }}></div>
-          <img onClick={handleUnfit} 
-            src={pic.src} 
-            alt={props.name} 
-            style={{ 
-              position:'absolute',
-              top:'0',
-              zIndex:'1000',
-              margin:'auto',
-              maxWidth:'calc(100vw - 2em)',
-              maxHeight:'100vh',
-              height:'auto',
-              marginRight:'1em',
-              cursor:'pointer',
-            }} />
+          <BackPlate/>
+          <Aligner>
+            <FittedImage 
+              onClick={handleUnfit} 
+              src={pic.src} 
+              alt={props.name} 
+              />
+          </Aligner>
         </>
          :
-        <div style={{
-            width: 'fit-content',
-            textAlign: 'center',
-            height: 'calc(100vh/3 - 4em/3)',
-            margin: '1em 1em 0 0',
-          }}
-          >
-            <img onClick={handleFit}
+        <TileImageContainer>
+            <TileImage onClick={handleFit}
               src={pic.src} 
               alt={props.name} 
               title={props.details}
-              style={{ 
-                borderRadius: '0 1em 0 0',
-                height: 'calc(100vh/3 - 4em/3 - 1.7em)',
-                cursor:'pointer'
-              }} />
+              />
             <InfoPanel {...props} />
-        </div>
+        </TileImageContainer>
 }
 
 export default MyImage
